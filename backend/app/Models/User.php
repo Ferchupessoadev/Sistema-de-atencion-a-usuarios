70095ea15cdef882f4125b8e15dfe450a29620ee
Thiago <Thiagosarlo01@gmail.com>
2026-08-24 08:02:51 -0300

    Ajustes en la parte de registro de usuario

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,12 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasPermissions;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     protected $table = 'users';
 
